57f2872fc5f11566315fe306e7c93e3763cd5712
Rajashekar94 <43386967+Rajashekar94@users.noreply.github.com>
2019-08-09 15:26:32 +0530

    Update Addition.java

--- src/main/java/com/company/helloworld/Addition.java
+++ src/main/java/com/company/helloworld/Addition.java
@@ -1,24 +1,14 @@
 public class Addition {
   public int num1;
   public int num2;
+ 
 //this is for adding two numbers
   public Addition(int num1, int num2) {
     this.num1 = num1;
     this.num2 = num2;
+    this.num2 = num2;
   }
 
-  public int getSum() {
-    return num1 + num2;
-  }
-public int getSum() {
-    return num1 + num2;
-  }
-  public int getSum() {
-    return num1 + num2;
-  }
-  public int getSum() {
-    return num1 + num2;
-  }
   public int getSum() {
     return num1 + num2;
   }
